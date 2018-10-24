dae1dc86c92138d47d274eb825b77a4ebbfb43b5
Prabhleen Kaur <35703655+Prabhleen9Kaur@users.noreply.github.com>
2018-10-24 17:52:55 -0400

    Delete connect.py

--- engine/connect.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://system_metrics:system_metrics@connections-shard-00-00-pdrg1.mongodb.net:27017,connections-shard-00-01-pdrg1.mongodb.net:27017,connections-shard-00-02-pdrg1.mongodb.net:27017/test?ssl=true&replicaSet=Connections-shard-0&authSource=admin&retryWrites=true")
-
-print(client.Connections)
